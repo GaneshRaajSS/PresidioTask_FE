b9f6a3dc1942cfb2c50169b703f2d0af1ff4ee64
Rahavendhor Saravanan <https://github.com/GaneshRaajSS/Angular_Form_learn>
2023-11-19 23:54:34 +0530

    secondss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import {Router} from '@angular/router'
-// import {}} from '@fortawesome/angular-fontawesome'
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
@@ -26,7 +25,6 @@ import { InspirationComponent } from './inspiration/inspiration.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule,
     ReactiveFormsModule,
     FormsModule
   ],
